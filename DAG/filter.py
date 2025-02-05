15586aa88151c26a0e8fa3ee5b32773431285d0e
Krishnabandi7 <103192820+Krishnabandi7@users.noreply.github.com>
2025-02-05 13:10:16 -0600

    Update filter.py

--- DAG/filter.py
+++ DAG/filter.py
@@ -3,7 +3,10 @@ from datetime import datetime
 
 def filter_function():
     print("Apply Filter condition")
+    #read file from the location
     df=pd.read_csv("in_file/Athletes_cleaned.csv")
+    #filter the data we want
     df= df[df['PersonName'].str.startswith("A")]
+    #store the Transformed data in the form of csv file in some location
     df.to_csv("ot_file/Athletes_filtered.csv")
     print("file is cleaned and date is added in file")
